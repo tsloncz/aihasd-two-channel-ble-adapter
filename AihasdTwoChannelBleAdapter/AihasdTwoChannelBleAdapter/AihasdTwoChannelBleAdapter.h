3c2aa9efd90e5aa2887b6ca2ec6ec9ba9fac2bd1
tsloncz <tsloncz@gmail.com>
2018-02-02 08:16:49 -0600

    Added framework project

---
+++ AihasdTwoChannelBleAdapter/AihasdTwoChannelBleAdapter/AihasdTwoChannelBleAdapter.h
@@ -0,0 +1,19 @@
+//
+//  AihasdTwoChannelBleAdapter.h
+//  AihasdTwoChannelBleAdapter
+//
+//  Created by Tim Sloncz on 2/2/18.
+//  Copyright © 2018 timsloncz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AihasdTwoChannelBleAdapter.
+FOUNDATION_EXPORT double AihasdTwoChannelBleAdapterVersionNumber;
+
+//! Project version string for AihasdTwoChannelBleAdapter.
+FOUNDATION_EXPORT const unsigned char AihasdTwoChannelBleAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AihasdTwoChannelBleAdapter/PublicHeader.h>
+
+
